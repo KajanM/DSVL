e459aecf5bcaf30340060d18cf60619a37d18328
K4J4N <kajan.14@cse.mrt.ac.lk>
2018-10-16 16:46:19 +0530

    added flood module

---
+++ flood/src/main/java/com/dsvl/flood/FloodApplication.java
@@ -0,0 +1,12 @@
+package com.dsvl.flood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloodApplication.class, args);
+    }
+}
