8cfd9ce40fc7903cecad17f932271e850c3a549a
thilan7 <you@example.com>
2018-11-30 15:58:25 +0530

    error correction in search

--- flood/src/main/java/com/dsvl/flood/MessageObject.java
+++ flood/src/main/java/com/dsvl/flood/MessageObject.java
@@ -6,11 +6,59 @@ public class MessageObject {
     private String msgType;
     private List<Neighbour> existingNodes;
     private Neighbour joinRequester;
+
     private String file_name;
     private int hops;
     private String ip;
     private int tcpPort;
     private int no_of_results;
+    private String search_ip;
+    private int search_udp_Port;
+    private int search_tcp_Port;
+    private int senderPort;
+    private String senderIP;
+    private String search_result_ip;
+    private int search_result_tcp_Port;
+
+
+
+
+    public String getSearch_ip() {
+        return search_ip;
+    }
+
+    public int getSearch_udp_Port() {
+        return search_udp_Port;
+    }
+
+    public void setSearch_ip(String search_ip) {
+        this.search_ip = search_ip;
+    }
+
+    public void setSearch_udp_Port(int search_udp_Port) {
+        this.search_udp_Port = search_udp_Port;
+    }
+
+
+
+
+    public String getSenderIP() {
+        return senderIP;
+    }
+
+    public void setSenderIP(String senderIP) {
+        this.senderIP = senderIP;
+    }
+
+
+    public int getSenderPort() {
+        return senderPort;
+    }
+
+    public void setSenderPort(int senderPort) {
+        this.senderPort = senderPort;
+    }
+
 
 
     public String getMsgType() {
@@ -73,5 +121,29 @@ public class MessageObject {
 
     public void setNo_of_results(int no_of_results) {
         this.no_of_results = no_of_results;
+    }
+
+    public int getSearch_tcp_Port() {
+        return search_tcp_Port;
+    }
+
+    public void setSearch_tcp_Port(int search_tcp_Port) {
+        this.search_tcp_Port = search_tcp_Port;
+    }
+
+    public String getSearch_result_ip() {
+        return search_result_ip;
+    }
+
+    public void setSearch_result_ip(String search_result_ip) {
+        this.search_result_ip = search_result_ip;
+    }
+
+    public int getSearch_result_tcp_Port() {
+        return search_result_tcp_Port;
+    }
+
+    public void setSearch_result_tcp_Port(int search_result_tcp_Port) {
+        this.search_result_tcp_Port = search_result_tcp_Port;
     }
 }
--- flood/src/test/java/com/dsvl/flood/NodeTest.java
+++ flood/src/test/java/com/dsvl/flood/NodeTest.java
@@ -2,6 +2,10 @@ package com.dsvl.flood;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
 
@@ -11,18 +15,39 @@ public class NodeTest {
 
     @Test
     public void search() {
+//        try {
+//            Node node=new Node("127.0.0.1",55555,   "DSVL",4500,45555);
+//            MessageObject msgObj=new MessageObject();
+//            msgObj.setHops(1);
+//            msgObj.setFile_name("of_mario");
+//            List<File> search_results=node.search(msgObj);
+//            for (File search_result : search_results) {
+//                System.out.println(search_result.getFileName());
+//            }
+//            assertEquals("Lord of the Rings",search_results.get(0).getFileName());
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        }
+        byte[] buf = "0047 SER 127.0.0.1 44444 Lord_of_the_rings_2_hacking 0".getBytes();
+        DatagramPacket packet = null;
         try {
-            Node node=new Node("127.0.0.1",55555,   "DSVL",4500,45555);
-            MessageObject msgObj=new MessageObject();
-            msgObj.setHops(1);
-            msgObj.setFile_name("of_mario");
-            List<File> search_results=node.search(msgObj);
-            for (File search_result : search_results) {
-                System.out.println(search_result.getFileName());
-            }
-            assertEquals("Lord of the Rings",search_results.get(0).getFileName());
+
+            packet = new DatagramPacket(buf, buf.length, InetAddress.getByName("127.0.0.1"), 44445);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.send(packet);
+            System.out.printf("message sent");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep((long) 5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
